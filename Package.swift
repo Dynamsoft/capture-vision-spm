43d91bf3b2f1a147fe6d8e49ce9cee7c8add4cf6
DMGithubPublisher <github@dynamsoft.com>
2025-03-07 09:18:05 +0800

    update to 2.6.1004

--- Package.swift
+++ Package.swift
@@ -21,14 +21,13 @@ let package = Package(
         .library(name: "DynamsoftLabelRecognizer", targets: ["DynamsoftLabelRecognizer"]),
         .library(name: "DynamsoftMRZ", targets: ["DynamsoftMRZ"]),
         .library(name: "DynamsoftVIN", targets: ["DynamsoftVIN"]),
-        .library(name: "DynamsoftCaptureVisionBundle", targets: ["DynamsoftCaptureVisionBundle"]),
     ],
     dependencies: [
     ],
     targets: [
         .binaryTarget(name: "DynamsoftCore", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-core-ios-3.4.30-xcframework.zip", checksum: "38bc98b783f8523fb2d89d635ec0a21fddccaaf641429f932c526809cc96598e"),
         .binaryTarget(name: "DynamsoftLicense", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-license-ios-3.4.40-xcframework.zip", checksum: "7516ba9c5d6e330885b738031fd76fa5ef46c1167d159b5aac703a9ca412f62d"),
-        .binaryTarget(name: "DynamsoftCameraEnhancer", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-cameraenhancer-ios-4.2.23-xcframework.zip", checksum: "d4b16f42358a9d706da80049b994ed214a54aa6fa1904ea52f2265246b808a24"),
+        .binaryTarget(name: "DynamsoftCameraEnhancer", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-cameraenhancer-ios-4.2.24-xcframework.zip", checksum: "5b4155e29c4279625a6615ea43dd4667d99457e05df339a735baa95677b620ba"),
         .binaryTarget(name: "DynamsoftImageProcessing", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-imageprocessing-ios-2.4.30-xcframework.zip", checksum: "94cded5f38b3c2d001108250e63311aabbae536e2265384b75335986ebaf50a7"),
         .binaryTarget(name: "DynamsoftBarcodeReader", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-barcodereader-ios-10.4.30-xcframework.zip", checksum: "04730bdcf5a56b03eab9374af381dbaa172c63c1aee9d577b461c57f168fee65"),
         .binaryTarget(name: "DynamsoftUtility", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-utility-ios-1.4.30-xcframework.zip", checksum: "22886725562e2c82d8f20ee80f06b45efabd58b41fbfe54dbd7b1f8d7e5a6d5b"),
@@ -40,7 +39,6 @@ let package = Package(
         .binaryTarget(name: "DynamsoftLabelRecognizer", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-labelrecognizer-ios-3.4.30-xcframework.zip", checksum: "13b95adee53ab02ce18c0d1b9221408442ba33a898fce760054215160d1c6b5b"),
         .binaryTarget(name: "DynamsoftMRZ", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-mrz-ios-3.4.20-xcframework.zip", checksum: "2c9b2fc46dbfd5b31b38cb1c7d91090dcea3e7d96917175f0ec25cdeb15a8303"),
         .binaryTarget(name: "DynamsoftVIN", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-vin-ios-3.4.20-xcframework.zip", checksum: "8d59034f8d4cebf9e33b9c8279aa8d8d8ad123fb1a24da8f4e5d8aa8bb2ea926"),
-        .binaryTarget(name: "DynamsoftCaptureVisionBundle", url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-capturevision-bundle-ios-2.6.1004-xcframework.zip", checksum: "ebac00798eada3a8853d2499c3a114cc3eaff61dbb77ddbe434f7ff9ec642686"),
     ]
 )
 
